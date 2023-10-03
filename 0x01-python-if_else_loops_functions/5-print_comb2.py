b04baa8026f6d62e2a39544cacafb0074a48df88
Muzey Tsigab <muzeyts@gmail.com>
2023-10-03 15:24:05 -0700

    correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 for nums in range(0, 100):
     if nums != 99:
         print("{:02d}, ".format(nums), end='')
-        else:
-            print("{:02d}".format(nums))
+    else:
+        print("{:02d}".format(nums))
