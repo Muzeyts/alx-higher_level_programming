91c965f0f1df4fc4e0a83e21a1a5de712a72c1b6
Muzey Tsigab <muzeyts@gmail.com>
2023-10-03 05:02:31 -0700

    Q4-5

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        if ch != 101 and ch != 113:
-                print("{:c}".format(ch), end='')
+for soup in range(97, 123):
+        if soup != 101 and soup != 113:
+                print("{:c}".format(soup), end='')
