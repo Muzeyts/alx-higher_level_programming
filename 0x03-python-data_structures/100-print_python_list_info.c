a14beffd3ce3503338b69a08d376de29e66f41a8
Muzey Tsigab <muzeyts@gmail.com>
2023-10-09 14:54:50 -0700

    comm

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - this prints python list info
+ *
+ * @p: Python Object
+ * Return: there is no return
+ */
+void print_python_list_info(PyObject *p)
+{
+
+}
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - this checks if a singly linked list is a palindrome
+ * @head: which pointer to head of list
+ * Return: returns 1 if it is a palndrome, and 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+
+}
