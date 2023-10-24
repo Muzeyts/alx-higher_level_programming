a7d5f246501526f7087600585fceefa82956c661
Muzey Tsigab <muzeyts@gmail.com>
2023-10-24 14:59:47 -0700

    commit

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+class Node:
+    def __init__(self, data, next_node=None):
---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+class Square:
+    def __eq__(self, other):
+        return self.__size == other.__size
+
