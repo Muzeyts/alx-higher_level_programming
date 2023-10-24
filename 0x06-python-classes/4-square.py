1d6c08095f863514330254bacc772e8bfab05bcd
Muzey Tsigab <muzeyts@gmail.com>
2023-10-24 16:13:47 -0700

    access square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,6 +6,18 @@ class Square:
     """
     Class representing a square
     """
+
+   def size(self, value):
+        """" 
+	Initializes a Square object with given size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
     def __init__(self, size=0):
         """" 
 	Initializes a Square object with given size
@@ -17,25 +29,15 @@ class Square:
         else:
             self.__size = size
 
+     def size(self):
+        """" 
+	Initializes a Square object with given size
+        """
+        return self.__size
+    @size.setter
+    
     def area(self):
         """" 
 	Initializes a Square object with given size
         """
     @property
-    def size(self):
-        """" 
-	Initializes a Square object with given size
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """" 
-	Initializes a Square object with given size
-        """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
