4badd0c3ec057c83d8954e016ebc36b7885c1b9d
Muzey Tsigab <muzeyts@gmail.com>
2023-11-06 15:19:27 -0800

    inheritance

---
+++ 0x0A-python-inheritance/tests/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ Class that freindsm Rectangle class """
+    def __init__(self, size):
+        """ Method it works es a Square """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+    def area(self):
+        """ Method tt yeas with the area """
+        return super().area()
