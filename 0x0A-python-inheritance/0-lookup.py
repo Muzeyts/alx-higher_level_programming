93912c9bda853608efd0ae104168966a2a30905f
Muzey Tsigab <muzeyts@gmail.com>
2023-11-06 15:51:05 -0800

    modi

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,14 @@
-#!/usr/bin/python3
 def lookup(obj):
-    """ Function which returns ls of  attributes
-    Args:
-        obj: instance ov z class
-    Returns:
-        List ov  attribts
     """
-    return dir(obj)
+    Return a list containing all the attributes and methods of an object.
+
+    Args:
+        obj (object): The object to inspect.
+
+    Returns:
+        list: A list containing all the attributes and methods of the object.
+    """
+
+    attrs = dir(obj)
+    methods = [attr for attr in attrs if callable(getattr(obj, attr))]
+    return attrs + methods
