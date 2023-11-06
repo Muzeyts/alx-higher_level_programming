1a8a667675a4c43429ef2aa088ddc9b9aa8c0ac9
Muzey Tsigab <muzeyts@gmail.com>
2023-11-06 14:45:14 -0800

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """ Function which returns ls of  attributes
+    Args:
+        obj: instance ov z class
+    Returns:
+        List ov  attribts
+    """
+    return dir(obj)
