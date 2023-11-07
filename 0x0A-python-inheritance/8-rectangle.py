9207b6110671d1921a7a264f8a4b5bec397dd6dc
Muzey Tsigab <muzeyts@gmail.com>
2023-11-07 05:29:54 -0800

    rectangue

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+'''8-base_geometry.py'''
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
-    """ Cimports  froegle from BaseGeometry Class """
+    '''class Rectangle inherits from BaseGeometry'''
+
     def __init__(self, width, height):
-        """ Initialize teachers instance """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        '''private instantiation of attributes
+           validate type with integer_validator
+        '''
+        super().integer_validator('width', width)
         self.__width = width
+        super().integer_validator('height', height)
         self.__height = height
